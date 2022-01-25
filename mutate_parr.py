71ef2b23bb6d53748a7d3a35157209e39af6093c
Priyansh Kedia <kediapriyansh@gmail.com>
2022-01-25 12:49:29 +0530

    changes

--- mutate_parr.py
+++ mutate_parr.py
@@ -14,8 +14,6 @@ from rdkit.Chem import MolFromSmiles as smi2mol
 from rdkit.Chem import MolToSmiles as mol2smi
 from selfies import encoder, decoder 
 
-# global manager, lock
-
 manager = multiprocessing.Manager()
 lock = multiprocessing.Lock()
 
@@ -44,18 +42,15 @@ def mutate_sf(sf_chars, alphabet):
     '''
     Given a list of SELFIES alphabets, make random changes to the molecule using 
     alphabet. Opertations to molecules are character replacements, additions and deletions. 
-
     Parameters
     ----------
     sf_chars : (list of string alphabets)
         List of string alphabets for a SELFIE string.
     alphabet : (list of SELFIE strings)
         Replacements and addition operations are performed using this list of SELFIE strings.
-
     Returns
     -------
     Muatted SELFIE string.
-
     '''
     random_char_idx = random.choice(range(len(sf_chars)))
     choices_ls = [1, 2, 3] # 1 = replacement; 2 = addition; 3=delete
@@ -91,7 +86,6 @@ def get_prop_material(smile, alphabet, num_random_samples, num_mutations):
     Given an input smile, perform mutations to the strucutre using provided SELFIE
     alphabet list. 'num_random_samples' number of different SMILES orientations are 
     considered & total 'num_mutations' are performed. 
-
     Parameters
     ----------
     smile : (str)
@@ -102,7 +96,6 @@ def get_prop_material(smile, alphabet, num_random_samples, num_mutations):
         Number of different SMILES orientations to be formed for the input smile.
     num_mutations : TYPE
         Number of mutations to perform on each of different orientations SMILES.
-
     Returns
     -------
     mutated_smiles_canon : (list of strings)
@@ -128,10 +121,7 @@ def get_prop_material(smile, alphabet, num_random_samples, num_mutations):
             if i == 0:  mutated_sf.append(mutate_sf(sf_chars, alphabet))
             else:       mutated_sf.append(mutate_sf ( get_selfie_chars(mutated_sf[-1]), alphabet ))
             
-    try:
-        mutated_smiles = [decoder(x) for x in mutated_sf]
-    except Exception as e:
-        return []
+    mutated_smiles = [decoder(x) for x in mutated_sf]    
     mutated_smiles_canon = []
     for item in mutated_smiles: 
         try: 
@@ -148,12 +138,10 @@ def get_prop_material(smile, alphabet, num_random_samples, num_mutations):
 def sanitize_smiles(smi):
     '''
     Return a canonical smile representation of smi 
-
     Parameters
     ----------
     smi : str
         smile string to be canonicalized 
-
     Returns
     -------
     mol (rdkit.Chem.rdchem.Mol) : 
@@ -174,7 +162,6 @@ def sanitize_smiles(smi):
 def get_chunks(arr, num_processors, ratio):
     '''
     Split list of SMILES int sublists, each of which will be operated on seperate cpus. 
-
     Parameters
     ----------
     arr : (list of sts)
@@ -183,7 +170,6 @@ def get_chunks(arr, num_processors, ratio):
         Number of cpus available for conducting operation.
     ratio : (int)
         number of operations that will be performed on each cpu.
-
     Returns
     -------
     chunks: (list of lists)
@@ -213,22 +199,17 @@ def calc_parr_prop(unseen_smile_ls, property_name, props_collect, num_random_sam
 def create_parr_process(chunks, alphabet, property_name, num_random_samples, num_mutations):
     '''
     Create parallel processes for creating mutating molecules for molecule in sublist chunks. 
-
     Parameters
     ----------
     chunks : (list of list)
         list of lists containing SMILES.
     property_name : (syr)
         optional name paramtere to enter.
-
     Returns
     -------
     combined_dict : (dict)
         input smiles -> [List of mutated smiles].
-
     '''
-    # manager = multiprocessing.Manager()
-    # lock = multiprocessing.Lock()
     process_collector    = []
     collect_dictionaries = []
         
@@ -240,10 +221,9 @@ def create_parr_process(chunks, alphabet, property_name, num_random_samples, num
         
         if property_name == 'logP':
             process_collector.append(multiprocessing.Process(target=calc_parr_prop, args=(item, property_name, props_collect, num_random_samples, num_mutations, alphabet, )))   
-
+    
     for item in process_collector:
-        print("called here")
-        item.start()       
+        item.start()
     
     for item in process_collector: # wait for all parallel processes to finish
         item.join()   
@@ -274,26 +254,5 @@ def get_mutated_smiles(smiles, alphabet, space='Explore'):
     
     
 if __name__ == '__main__': 
-    # global manager, lock
-    # manager = multiprocessing.Manager()
-    # lock = multiprocessing.Lock()
     molecules_here        = ['CCC', 'CCCC', 'CCCCC', 'CCCCCCCC', 'CS', 'CSSS', 'CSSSSS', 'CF', 'CI', 'CBr', 'CSSSSSSSSSSSS', 'CSSSSSSSSSC', 'CSSSSCCSSSC', 'CSSSSSSSSSF', 'SSSSSC']
     A = get_mutated_smiles(molecules_here, alphabet=['[C]']*500, space='Explore')
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
